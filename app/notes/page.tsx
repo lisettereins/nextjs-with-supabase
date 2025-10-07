9af03744079f52a78eb7941e5db45e0433a5004e
lisettereins <lreins@tlu.ee>
2025-10-07 17:12:22 +0300

    fix: turned the notes read page into a server component

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,26 +1,16 @@
-'use client'
 
 import { createClient } from '@/lib/supabase/client'
-import { useEffect, useState } from 'react'
 
-type Note = {
-  id: number
-  title: string
-}
 
-export default function Page() {
-  const [notes, setNotes] = useState<Note[] | null>(null)
-  
 
-  useEffect(() => {
+export default async function Page() {
+
     const supabase = createClient()
 
-    const getData = async () => {
-      const { data } = await supabase.from('notes').select()
-      setNotes(data)
-    }
-    getData()
-  }, [])
+    
+      const { data: notes } = await supabase.from('notes').select()
+      
+
 
   return (
     <div className='mx-auto flex-col max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10'>

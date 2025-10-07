0fe3a095bd209b49f9bf8ee731f51471a5ee678d
lisettereins <lreins@tlu.ee>
2025-10-07 22:20:51 +0300

    fix: fixed main page header

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -29,7 +29,7 @@ export default function Page() {
   return (
     <div className="mx-auto flex-col max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
       <div className="flex items-center justify-between mb-4">
-        <h2 className="text-3xl font-bold text-black dark:text-white">Notes</h2>
+        <h2 className="text-3xl font-bold text-black dark:text-white">To do</h2>
         <a href="./todos/create">
           <button className=" w-12 h-12 rounded-full border border-gray-300 bg-gray-100 hover:bg-gray-200 transition-colors duration-200">
             +

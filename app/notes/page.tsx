ae916bf4f4fe091c7246295d3800680fdb26e476
lisettereins <lreins@tlu.ee>
2025-10-07 21:08:55 +0300

    feat: added delete function to notes page

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,11 +1,33 @@
-import { createClient } from "@/lib/supabase/client";
+'use client'
 
-export default async function Page() {
-  const supabase = createClient();
+import { createClient } from '@/lib/supabase/client'
+import { useEffect, useState } from 'react'
+import { deleteNote } from './actions/deleteNote'
 
-  const { data: notes } = await supabase.from("notes").select();
+export default function Page() {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [notes, setNotes] = useState<any[] | null>(null)
+  const supabase = createClient()
 
-  return (
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await supabase.from('notes').select()
+      setNotes(data)
+    }
+    getData()
+  }, [supabase])
+
+  const handleDelete = async (noteId: string) => {
+    if (!confirm("Are you sure?")) {
+      return
+    }
+
+   await deleteNote(noteId)
+    setNotes(notes?.filter(note => note.id !== noteId) || null)
+    
+  }
+
+  return  (
     <div className="mx-auto flex-col max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
       <div className="flex items-center justify-between mb-4">
         <h2 className="text-3xl font-bold text-black dark:text-white">Notes</h2>
@@ -17,7 +39,7 @@ export default async function Page() {
       </div>
       <ul>
         {notes?.map((note) => (
-          <li key={note.id}>{note.title}</li>
+          <li key={note.id}>{note.title} <button onClick={() => handleDelete(note.id)}>🗑️</button></li>
         ))}
       </ul>
     </div>

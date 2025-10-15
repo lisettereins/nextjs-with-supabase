3a2fa8bbf48a173543f46928d065c5e5aed4ef80
lisettereins <lreins@tlu.ee>
2025-10-15 20:46:00 +0300

    feat: added server action for updating todos

---
+++ app/todos/[id]/edit/action.ts
@@ -0,0 +1,15 @@
+"use server";
+
+import { createClient } from "@/lib/supabase/server";
+
+
+export async function updateTodo(todoId: string, name: string) {
+    const supabase = await createClient();
+
+    const {error} = await supabase.from("todos").update({name}).eq("id", todoId);
+
+    if(error) {
+        throw new Error("update failed");
+    }
+    return {success: true}
+}
